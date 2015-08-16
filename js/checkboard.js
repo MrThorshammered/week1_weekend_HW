dfba09442d65b9b399bc01c93c51650d5b67d424
Mike Jones <mikethespike77@gmail.com>
2015-08-16 20:40:06 +0100

    trying to get inputs for a winner

--- js/checkboard.js
+++ js/checkboard.js
@@ -1,11 +1,24 @@
-// Changing players from x to o 
-function changePlayer(){
+// players so that the console knows who is who
+var player1 = "X";
+var player2 = "O";
+// Add and click event to the board div so that when a box is selected it assigns
+// an x or an o to that particular box. then watch the box so that values can be
+// toted up to get a combination from winning array
+var currentPlayer = document.getElementById('currentPlayer');
 
- currentPlayer === 'X' ? currentPlayer = 'O' : currentPlayer = 'X';
- document.getElementById('currentPlayer').innerHTML = currentPlayer;
-}
+var body = document.getElementById('board')
 
-// var currentPlayer = getElementById(currentPlayer);
+body.addEventListener('click', function(){
+ if (currentPlayer.innerHTML === "X"){
+  currentPlayer.innerHTML = "O";
+}else{
+   currentPlayer.innerHTML = "X";
+}});
+
+
+
+// getting information from the current player as so to alter which player is selected
+var playerChange = (currentPlayer.innerHTML.value);
 
 // declare all possible winning declerations
 var winningArray = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[1,4,7],[2,4,6],[0,3,6],[2,5,8]];
@@ -26,75 +39,83 @@ var movesArray;
  clickBox1.addEventListener("click", function(){
  console.log("box 1 was clicked");
    if (box1_1.innerHTML === 'X'){
-   	box1_1.innerHTML = 'O'
+   	box1_1.innerHTML = "O"
    }else{
-   	box1_1.innerHTML = 'X'
+   	box1_1.innerHTML = "X"
    } });
 
  clickBox2.addEventListener("click", function(){
  console.log("box 2 was clicked");
-if (currentPlayer.innerHTML === 'X'){
-   	box1_2.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box1_2.innerHTML = "O"
    }else{
-   	box1_2.innerHTML = 'X'
+   	box1_2.innerHTML = "X"
    } });
 
  clickBox3.addEventListener("click", function(){
  console.log("box 3 was clicked");
-if (box1_3.innerHTML === 'X'){
-   	box1_3.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box1_3.innerHTML = "O"
    }else{
-   	box1_3.innerHTML = 'X'
+   	box1_3.innerHTML = "X"
    } });
 
  clickBox4.addEventListener("click", function(){
  console.log("box 4 was clicked");
-if (box2_1.innerHTML === 'X'){
-   	box2_1.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box2_1.innerHTML = "O"
    }else{
-   	box2_1.innerHTML = 'X'
+   	box2_1.innerHTML = "X"
    } });
 
  clickBox5.addEventListener("click", function(){
  console.log("box 5 was clicked");
- if (box2_2.innerHTML === 'X'){
-   	box2_2.innerHTML = 'O'
+ if (currentPlayer.innerHTML === "X"){
+   	box2_2.innerHTML = "O"
    }else{
-   	box2_2.innerHTML = 'X'
+   	box2_2.innerHTML = "X"
    } });
 
  clickBox6.addEventListener("click", function(){
  console.log("box 6 was clicked");
-if (box2_3.innerHTML === 'X'){
-   	box2_3.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box2_3.innerHTML = "O"
    }else{
-   	box2_3.innerHTML = 'X'
+   	box2_3.innerHTML = "X"
    } });
 
  clickBox7.addEventListener("click", function(){
  console.log("box 7 was clicked");
-if (box3_1.innerHTML === 'X'){
-   	box3_1.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box3_1.innerHTML = "O"
    }else{
-   	box3_1.innerHTML = 'X'
+   	box3_1.innerHTML = "X"
    } });
  
  clickBox8.addEventListener("click", function(){
  console.log("box 8 was clicked");
-if (box3_2.innerHTML === 'X'){
-   	box3_2.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box3_2.innerHTML = "O"
    }else{
-   	box3_2.innerHTML = 'X'
+   	box3_2.innerHTML = "X"
    } });
 
  clickBox9.addEventListener("click", function(){
  console.log("box 9 was clicked");
-if (box3_3.innerHTML === 'X'){
-   	box3_3.innerHTML = 'O'
+if (currentPlayer.innerHTML === "X"){
+   	box3_3.innerHTML = "O"
    }else{
-   	box3_3.innerHTML = 'X'
+   	box3_3.innerHTML = "X"
    } });
 
+// declare a winner
+if (player1 === winningArray){
+	alert("X has won");
+}else if(player2 === winningArray){
+	alert("O has won");
+}else{
+	alert("its a tie");
+};
 
 // need an array to store clickEvent data
 // movesArray = movesArray.push[index]
@@ -102,9 +123,6 @@ if (box3_3.innerHTML === 'X'){
 
 
 
-// Add and click event to the board div so that when a box is selected it assigns
-// an x or an o to that particular box. then watch the box so that values can be
-// toted up to get a combination from winning array
 
 
 
